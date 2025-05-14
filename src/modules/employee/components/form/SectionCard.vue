0d226151180867661f23bf653d8ccb1dc6ed63d3
Brenox889 <brenomendes889@hotmail.com>
2025-05-14 01:08:03 -0300

    feat: Section card atom for app form

---
+++ src/modules/employee/components/form/SectionCard.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps<{ title: string }>()
+</script>
+
+<template>
+  <div class="bg-gray-50 p-4 rounded-md border border-gray-100">
+    <h2 class="text-sm uppercase font-semibold text-gray-500 mb-4">{{ title }}</h2>
+    <slot />
+  </div>
+</template>
