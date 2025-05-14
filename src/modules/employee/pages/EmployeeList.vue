3d18cb835de587a612ed185873a1fa3efe74c560
Brenox889 <brenomendes889@hotmail.com>
2025-05-14 01:22:20 -0300

    feat: EmployeList page to show employee data

---
+++ src/modules/employee/pages/EmployeeList.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="p-6">
+    <div class="mb-6">
+      <h1 class="text-2xl font-bold text-purple-800 mb-2">Employee Management</h1>
+    </div>
+    <EmployeeTable />
+  </div>
+</template>
+
+<script setup lang="ts">
+import EmployeeTable from '../components/table/EmployeeTable.vue'
+</script>
