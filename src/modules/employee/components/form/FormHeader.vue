6770d60bcd972ca0291c863df0e3fb1eb0dfeb10
Brenox889 <brenomendes889@hotmail.com>
2025-05-14 01:07:32 -0300

    feat: form atom header

---
+++ src/modules/employee/components/form/FormHeader.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{ isEditing: boolean }>()
+</script>
+
+<template>
+  <div class="bg-gradient-to-r from-purple-600 to-purple-500 px-6 py-4 text-white">
+    <h1 class="text-xl font-bold flex items-center">
+      <slot name="icon" />
+      {{ isEditing ? 'Edit Employee' : 'New Employee' }}
+    </h1>
+  </div>
+</template>
