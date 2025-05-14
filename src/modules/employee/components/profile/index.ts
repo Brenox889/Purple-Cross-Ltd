308bcad3bbc8e96b515f33c096312c0db2e8c650
Brenox889 <brenomendes889@hotmail.com>
2025-05-14 01:14:10 -0300

    chore: export all profile atoms components

---
+++ src/modules/employee/components/profile/index.ts
@@ -0,0 +1,4 @@
+export { default as ProfileHeader } from './ProfileHeader.vue'
+export { default as AvatarCard } from './AvatarCard.vue'
+export { default as InfoForm } from './InfoForm.vue'
+export { default as StatusPanel } from './StatusPanel.vue'
