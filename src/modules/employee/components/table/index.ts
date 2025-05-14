7ce6ea70c587086494a8b8d65ee9bcc4719402a6
Brenox889 <brenomendes889@hotmail.com>
2025-05-14 01:15:30 -0300

    chore: export all table components

---
+++ src/modules/employee/components/table/index.ts
@@ -0,0 +1,4 @@
+export { default as EmployeeTable } from './EmployeeTable.vue'
+export { default as EmployeeTableRow } from './EmployeeTableRow.vue'
+export { default as EmployeeTableHeader } from './EmployeeTableHeader.vue'
+export { default as EmployeeTableEmpty } from './EmployeeTableEmpty.vue'
